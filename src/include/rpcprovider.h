ca450b4bf460e0c724c16fdc34387472fde9e3f7
05allan1213 <2787554820@qq.com>
2025-06-01 15:38:57 +0800

    rpc服务注册方法实现

--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <google/protobuf/service.h>
-#include <memory>
 #include <muduo/net/TcpServer.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <muduo/net/TcpConnection.h>
+#include <google/protobuf/descriptor.h>
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <iostream>
 
 // 框架提供的专门发布rpc服务的类
 class RpcProvider
@@ -20,6 +24,17 @@ public:
 private:
     // 组合的muduo库的线程池
     muduo::net::EventLoop m_eventLoop;
+
+    // service服务类型信息
+    struct ServiceInfo
+    {
+        google::protobuf::Service *m_service;                                                    // 保存服务对象
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor *> m_methodMap; // 保存服务方法的映射关系
+    };
+
+    // 保存注册成功的服务对象和其方法的映射关系
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
+
     // 上报连接信息的回调函数
     void OnConnection(const muduo::net::TcpConnectionPtr &conn);
     // 上报读写事件信息的回调函数
--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,11 +1,27 @@
 #include "rpcprovider.h"
-#include <string>
 #include "mprpcapplication.h"
-#include <functional>
-#include <iostream>
 
 void RpcProvider::NotifyService(google::protobuf::Service *service)
 {
+    ServiceInfo serviceInfo;
+    serviceInfo.m_service = service;
+    // 获取服务对象的描述信息
+    const google::protobuf::ServiceDescriptor *serviceDescriptor = service->GetDescriptor();
+    // 获取服务的名字
+    std::string service_name = std::string(serviceDescriptor->name());
+    // 获取服务对象service的方法的数量
+    int methodCount = serviceDescriptor->method_count();
+    std::cout << "service_name: " << service_name << std::endl;
+    std::cout << "methodCount: " << methodCount << std::endl;
+
+    for (int i = 0; i < methodCount; ++i)
+    {
+        // 获取方法的描述信息
+        const google::protobuf::MethodDescriptor *methodDescriptor = serviceDescriptor->method(i);
+        std::string method_name = std::string(methodDescriptor->name());
+        serviceInfo.m_methodMap.insert({method_name, methodDescriptor});
+    }
+    m_serviceMap.insert({service_name, serviceInfo});
 }
 
 void RpcProvider::Run()
